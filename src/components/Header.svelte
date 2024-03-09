89aabadc1ca8055449e83992420b26fdb42a3621
Al Hera Ahmad <alheraahmad0786@gmail.com>
2024-03-09 15:24:55 +0530

    feat: added products section

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,8 +8,16 @@
                 Club <span class="text-indigo-400">Fitness</span>
             </h1>
         </a>
-        <button>
+        <button class="md:hidden grid place-items-center">
             <i class="fa-solid fa-bars"></i>
         </button>
+        <nav class="hidden md:flex items-center gap-4 lg:gap-6">
+            <a href= "#products" class="duration-200 hover:text-indigo-400 cursor-pointer">Products</a>
+            <a href= "#reviews" class="duration-200 hover:text-indigo-400 cursor-pointer">reviews</a>
+            <a href= "#faqs" class="duration-200 hover:text-indigo-400 cursor-pointer">FAQs</a>
+            <button class="specialBtn">
+                <p>Start free trail today</p>
+            </button>
+        </nav>
     </div>
 </header>
