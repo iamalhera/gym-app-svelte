ca135777c04ba9e5e6da56ceab58a666491d7d62
Al Hera Ahmad <alheraahmad0786@gmail.com>
2024-03-09 11:28:25 +0530

    added basic svelte app

---
+++ src/components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<header class="flex flex-col relative z-1">
+    <div class="max-w-[1400px] mx-auto w-full flex items-center justify-between p-4 py-6">
+        <a href="/">
+            <h1 class="font-semibold">
+                Club <span class="text-indigo-400">Fitness</span>
+            </h1>
+        </a>
+        <button>
+            <i class="fa-solid fa-bars"></i>
+        </button>
+    </div>
+</header>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Hero from "../components/Hero.svelte";
+</script>
+<main class="flex flex-col">
+    <Hero />
+</main>
